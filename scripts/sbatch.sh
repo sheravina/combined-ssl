27d711406e54dcb54db333fd8d71ac34ac1a6561
Brillianda Sheravina <shera.assa@gmail.com>
2025-04-18 19:51:59 +0200

    update script sbatch

--- scripts/sbatch.sh
+++ scripts/sbatch.sh
@@ -4,11 +4,7 @@
 #SBATCH --output=./log/py_torch_test%j.log
 #SBATCH --error=./log/py_torch_test%j.err
 #SBATCH --partition=STUD
-#SBATCH --gres=gpu:1
 
 
 ./../miniconda3/bin/activate SRP_ENV
-srun python test.py
-#srun python main.py --dataset cifar10 --encoder resnet18 --model supervised --ssl simclr --opt sgd --bs 128 --eppt 150 --epft 50 --lr 0.01 --wd 0.0005 --seed 42
-#echo "This is a test echo. Watiting..."
-#echo "Done"
+srun python main.py --dataset cifar10 --encoder resnet18 --model supervised --ssl simclr --opt sgd --bs 128 --eppt 150 --epft 50 --lr 0.01 --wd 0.0005 --seed 42
